b06e23fa6b32e623fcbe57a9e7ccdf60858a2016
Mateusz Podlasin <podlas29@gmail.com>
2022-04-10 16:06:52 +0200

    poc of dymamically connecting nodes in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import './App.css';
 import React from 'react';
-import { useAudioContext, useOscillator, useGain, useConnect } from './lib/audio/hooks';
+import { useAudioContext } from './lib/audio/hooks';
+import { create } from 'domain';
 
 interface OscillatorProps {
   oscillator: OscillatorNode;
   audioContext: AudioContext;
+  top: number;
+  left: number;
+
+  onOutChosen(): void;
 }
 
-function Oscillator({ oscillator, audioContext }: OscillatorProps) {
+function Oscillator({ oscillator, audioContext, top, left, onOutChosen }: OscillatorProps) {
   React.useEffect(() => {
     oscillator.type = 'sine';
     oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
   }, [audioContext.currentTime, oscillator]);
 
-  return <div>Osc</div>;
+  return <div  onClick={e => e.stopPropagation()} style={{border: '1px solid gray', position: 'absolute', top, left, padding: 20,}}>
+    <div>{oscillator.type}</div>
+    <div onMouseDown={onOutChosen} style={{border: '1px solid gray'}}>Out</div>
+  </div>;
 }
 
 interface GainProps {
   gain: GainNode;
+  top: number;
+  left: number;
+  onInChosen(): void;
+  onOutChosen(): void;
 }
 
-function Gain({ gain }: GainProps) {
+function Gain({ gain, top, left, onInChosen }: GainProps) {
 
   React.useEffect(() => {
     gain.gain.value = 0;
@@ -30,7 +42,11 @@ function Gain({ gain }: GainProps) {
     gain.gain.value = e.currentTarget.valueAsNumber;
   }
 
-  return <input type="range" min={0} max={1} step={0.01} defaultValue={0} onInput={changeGain} />;
+  return <div onClick={e => e.stopPropagation()} style={{border: '1px solid gray', position: 'absolute', top, left, padding: 20,}}>
+    <div onMouseUp={onInChosen} style={{border: '1px solid gray'}}>In</div>
+    <input type="range" min={0} max={1} step={0.01} defaultValue={0} onInput={changeGain} />
+    <div style={{border: '1px solid gray'}}>Out</div>
+    </div>;
 }
 
 interface OutputProps {
@@ -68,25 +84,7 @@ function Output({ audioContext }: OutputProps) {
     </div>
 }
 
-function AudioGraph() {
-  const audioContext = useAudioContext();
-  const osc = useOscillator(audioContext);
-  const gain = useGain(audioContext);
-
-  useConnect(osc, gain);
-  useConnect(gain, audioContext.destination);
-
-  return (
-    <div>
-      <Oscillator oscillator={osc} audioContext={audioContext} />
-      <Gain gain={gain} />
-      <Output audioContext={audioContext} />
-    </div>
-  );
-};
-
 type NodeType = 'oscillator' | 'gain' | 'output';
-
 interface CreationMenuProps {
   top: number;
   left: number;
@@ -128,7 +126,8 @@ function AudioCanvas() {
   const [showCreationMenu, setShowCreationMenu] = React.useState(false);
   const [creationMenuPosition, setCreationMenuPosition] = React.useState({top: 0, left: 0});
 
-  const [oscillators, setOscillators] = React.useState<OscillatorNode[]>([]);
+  const [oscillators, setOscillators] = React.useState<{ oscillator: OscillatorNode, position: { left: number, top: number}}[]>([]);
+  const [gains, setGains] = React.useState<{ gain: GainNode, position: { left: number, top: number}}[]>([]);
 
   const audioContext = useAudioContext();
 
@@ -137,20 +136,89 @@ function AudioCanvas() {
       case 'oscillator':
         setOscillators([
           ...oscillators,
-          audioContext.createOscillator(),
-        ])
+          {
+            oscillator: audioContext.createOscillator(),
+            position: creationMenuPosition,
+          }
+        ]);
+        setShowCreationMenu(false);
         return;
       case 'gain':
+        setGains([
+          ...gains,
+          {
+            gain: audioContext.createGain(),
+            position: creationMenuPosition,
+          }
+        ]);
+        setShowCreationMenu(false);
+        return;
       case 'output':
     }
   };
 
+  const [createdConnection, setCreatedConnection] = React.useState<{inType: NodeType, inIndex: number}>();
+
+  const [connections, setConnections] = React.useState<{inType: NodeType, inIndex: number, outType: NodeType, outIndex: number}[]>([]);
+
+  const handleOscillatorOut = (i: number) => () => {
+    setCreatedConnection({
+      inType: 'oscillator',
+      inIndex: i,
+    });
+  }
+
+  const handleGainIn = (i: number) => () => {
+    console.log('handle gain in!!!!');
+
+    if (createdConnection) {
+      setConnections([
+        ...connections,
+        {
+          ...createdConnection,
+          outIndex: i,
+          outType: 'gain',
+        }
+      ]);
+    }
+  }
+
+  const [mousePositon, setMousePosition] = React.useState<{top: number, left: number}>();
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (createdConnection) {
+      setMousePosition({top: e.clientY, left: e.clientX});
+    }
+  }
+
+  const handleMouseUp = () => {
+    if (createdConnection) {
+      setCreatedConnection(undefined);
+    }
+  }
+
   return (
-    <div ref={ref} onClick={handleClick} style={{border: '1px solid red', position: 'relative', height: '100%'}}>
+    <div ref={ref} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onClick={handleClick} style={{border: '1px solid red', position: 'relative', height: '100%', boxSizing: 'border-box'}}>
       {showCreationMenu && <CreationMenu onCreate={handleCreate} {...creationMenuPosition} />}
-      {oscillators.map((oscillator, i) => <Oscillator key={i} oscillator={oscillator} audioContext={audioContext} />)}
+      {oscillators.map((oscillator, i) => <Oscillator key={i} {...oscillator.position} oscillator={oscillator.oscillator} audioContext={audioContext} onOutChosen={handleOscillatorOut(i)} />)}
+      {gains.map((gain, i) => <Gain key={i} {...gain.position} gain={gain.gain} onInChosen={handleGainIn(i)} onOutChosen={() => {}} />)}
+      {createdConnection && <Connection inTop={oscillators[createdConnection.inIndex].position.top} inLeft={oscillators[createdConnection.inIndex].position.left} outTop={mousePositon?.top ?? 0} outLeft={mousePositon?.left ?? 0} />}
+      {connections.map(connection => <Connection inTop={oscillators[connection.inIndex].position.top} inLeft={oscillators[connection.inIndex].position.left} outTop={gains[connection.outIndex].position.top} outLeft={gains[connection.outIndex].position.left} />)}
     </div>
   );
+}
+
+interface ConnectionProps {
+  inTop: number;
+  inLeft: number;
+  outTop: number;
+  outLeft: number;
+}
+
+function Connection({inTop, inLeft, outTop, outLeft,}: ConnectionProps) {
+  return <svg style={{pointerEvents: 'none', position: 'absolute', top: inTop, left: inLeft, height: outTop - inTop, width: outLeft - inLeft}} width="100" height="100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <line x1="0" y1="0" x2={outLeft - inLeft} y2={outTop - inTop} stroke="black" />
+  </svg>
 }
 
 function App() {
