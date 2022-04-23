f614cee049008d8d36f7fb6ea22498c4bd4c291b
Mateusz Podlasin <podlas29@gmail.com>
2022-04-23 20:36:24 +0200

    working poc of graph canvas, including edges

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { useAudioContext } from './lib/audio/hooks';
-import { ComponentGraphCanvas } from './lib/component-graph-canvas';
+import { ComponentGraphCanvas, EdgeEnd } from './lib/component-graph-canvas';
 
 interface OscillatorProps {
   oscillator: OscillatorNode;
@@ -224,12 +224,42 @@ function Connection({inTop, inLeft, outTop, outLeft,}: ConnectionProps) {
 function App() {
     return (
       <div style={{height: '100%'}}>
-        <ComponentGraphCanvas>
-          <div>First component</div>
-          <div>
-            <input type="range" />
-          </div>
-        </ComponentGraphCanvas>
+        <ComponentGraphCanvas
+          nodes={[
+            {
+              name: 'Oscillator',
+              component: <div>Type: saw</div>,
+              inPlugs: [
+                {
+                  type: 'audio',
+                  name: 'Input'
+                }
+              ],
+              outPlugs: [
+                {
+                  type: 'audio',
+                  name: 'Output'
+                }
+              ],
+            },
+            {
+              name: 'Gain',
+              component: <input type="range" />,
+              inPlugs: [
+                {
+                  type: 'audio',
+                  name: 'Input'
+                }
+              ],
+              outPlugs: [
+                {
+                  type: 'audio',
+                  name: 'Output'
+                }
+              ],
+            }
+          ]}
+        />
       </div>
     );
 }
--- src/lib/component-graph-canvas/index.tsx
+++ src/lib/component-graph-canvas/index.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import internal from "stream";
 
-interface ComponentGraphCanvasProps {
-    children?: React.ReactElement[];
+interface Plug {
+    type: string;
+    name: string;
 }
 
-export function ComponentGraphCanvas({ children }: ComponentGraphCanvasProps) {
-    const [positions, setPositions] = React.useState(React.Children.map(children, () => ({ top: 0, left: 0 })) ?? []);
-    const [dragging, setDragging] = React.useState<boolean[]>(React.Children.map(children, () => false) ?? []);
-    const [internalElementPositions, setInternalElementPositions] = React.useState<(null | { top: number; left: number })[]>(React.Children.map(children, () => null) ?? []);
+interface Position {
+    top: number;
+    left: number;
+}
+
+interface ComponentGraphCanvasPropsNode {
+    name: string;
+    component: React.ReactNode;
+    inPlugs: Plug[];
+    outPlugs: Plug[];
+}
+
+interface ComponentGraphCanvasProps {
+    nodes: ComponentGraphCanvasPropsNode[];
+}
+
+export function EdgeEnd() {
+    return <div style={{border: '1px solid black', backgroundColor: 'lightgray', height: '1rem', width: '1rem'}}></div>
+}
+
+export function ComponentGraphCanvas({ nodes }: ComponentGraphCanvasProps) {
+    const [positions, setPositions] = React.useState(nodes.map(() => ({ top: 0, left: 0 })));
+    const [plugPositions, setPlugPositions] = React.useState<Position[][]>(nodes.map(() => []));
+    const [dragging, setDragging] = React.useState<boolean[]>(nodes.map(() => false));
+    const [internalElementPositions, setInternalElementPositions] = React.useState<(null | { top: number; left: number })[]>(nodes.map(() => null));
 
     const handleDragStart = (i: number): React.MouseEventHandler<HTMLDivElement> => e => {
-        const target = e.target as HTMLElement;
-        if (!target.classList.contains('wrapper')) return;
-
         setDragging(dragging => {
             const draggingCopy = [...dragging];
 
@@ -37,16 +55,13 @@ export function ComponentGraphCanvas({ children }: ComponentGraphCanvasProps) {
     };
 
     const handleDragStop = () => {
-        setDragging(React.Children.map(children, () => false) ?? []);
+        setDragging(nodes.map(() => false));
     }
 
     const handleDrag: React.MouseEventHandler<HTMLDivElement> = e => {
-        const target = e.target as HTMLElement;
-        if (target.classList.contains('wrapper')) {
-            e.preventDefault();
-        }
-
         if (dragging.some(d => d === true)) {
+            e.preventDefault();
+
             const draggedIndex = dragging.indexOf(true);
 
             setPositions(positions => {
@@ -64,9 +79,164 @@ export function ComponentGraphCanvas({ children }: ComponentGraphCanvasProps) {
         }
     };
 
-    return <div onMouseUp={handleDragStop} onMouseMove={handleDrag} style={{position: 'relative', border: '1px solid red', height: '100%', boxSizing: 'border-box'}}>{React.Children.map(children, (child, i) => 
-        <div className="wrapper" onMouseDown={handleDragStart(i)} style={{border: '1px solid gray', position: 'absolute', padding: 20, cursor: 'move', ...positions[i]}}>
-            <div style={{cursor: 'initial', border: '1px solid lightgray', padding: 20}}>{child}</div>
+    const [createdConnection, setCreatedConnection] = React.useState<{
+        inNodeIndex: number, 
+        inPlugIndex: number,
+        inPosition: Position;
+        outPosition: Position;
+    }>();
+
+    const handleStartConnecting = (nodeIndex: number) => (plugIndex: number, position: Position) => {
+        setCreatedConnection({
+                inNodeIndex: nodeIndex,
+                inPlugIndex: plugIndex,
+                inPosition: position,
+                outPosition: position,
+            });
+    };
+
+    const handleConnectingDrag: React.MouseEventHandler<HTMLDivElement> = e => {
+        if (createdConnection) {
+            setCreatedConnection({
+                ...createdConnection,
+                outPosition: {
+                    top: e.clientY,
+                    left: e.clientX,
+                }
+            })
+        }
+    };
+
+    const handleCancelConnecting = () => {
+        setCreatedConnection(undefined);
+    };
+
+
+    const [connections, setConnections] = React.useState<{
+        inNodeIndex: number,
+        inPlugIndex: number,
+        outNodeIndex: number,
+        outPlugIndex: number,
+    }[]>([]);
+
+    const handleStopConnecting = (nodeIndex: number) => (plugIndex: number, position: Position) => {
+
+        if (createdConnection) {
+            setConnections(connections => [...connections, {
+                inNodeIndex: createdConnection.inNodeIndex,
+                inPlugIndex: createdConnection.inPlugIndex,
+                outNodeIndex: nodeIndex,
+                outPlugIndex: plugIndex,
+            }]);
+
+            setCreatedConnection(undefined);
+        }
+    };
+
+    const handlePlugPositions = (nodeIndex: number) => (newPlugPositions: Position[]) => {
+        setPlugPositions(plugPositions => {
+            const plugPositionsCopy = [...plugPositions];
+
+            plugPositionsCopy[nodeIndex] = newPlugPositions;
+
+            return plugPositionsCopy;
+        });
+    };
+
+    return <div onMouseUp={() => { handleDragStop(); handleCancelConnecting(); }} onMouseMove={e => { handleDrag(e); handleConnectingDrag(e); }} style={{position: 'relative', border: '1px solid red', height: '100%', boxSizing: 'border-box'}}>
+        {nodes.map((node, i) => <ComponentGraphCanvasNode
+            key={i}
+            node={node} 
+            position={positions[i]} 
+            onDragStart={handleDragStart(i)} 
+            onStartConnecting={handleStartConnecting(i)}
+            onStopConnecting={handleStopConnecting(i)}
+            onPlugPositions={handlePlugPositions(i)}
+        />)}
+        <svg style={{width: '100%', height: '100%', pointerEvents: 'none'}}>
+            {createdConnection && <line x1={plugPositions[createdConnection.inNodeIndex][createdConnection.inPlugIndex].left + positions[createdConnection.inNodeIndex].left} y1={plugPositions[createdConnection.inNodeIndex][createdConnection.inPlugIndex].top + positions[createdConnection.inNodeIndex].top} x2={createdConnection.outPosition.left} y2={createdConnection.outPosition.top} stroke="black" />}
+            {connections.map(connection => <line 
+                x1={plugPositions[connection.inNodeIndex][connection.inPlugIndex].left + positions[connection.inNodeIndex].left} 
+                y1={plugPositions[connection.inNodeIndex][connection.inPlugIndex].top + positions[connection.inNodeIndex].top} 
+                x2={plugPositions[connection.outNodeIndex][connection.outPlugIndex].left + positions[connection.outNodeIndex].left} 
+                y2={plugPositions[connection.outNodeIndex][connection.outPlugIndex].top + positions[connection.outNodeIndex].top} 
+                stroke="black"
+            />)}
+        </svg>
+    </div>;
+}
+
+interface ComponentGraphCanvasNodeProps {
+    node: ComponentGraphCanvasPropsNode;
+    position: Position;
+    onDragStart(e: React.MouseEvent<HTMLDivElement>): void;
+    onStartConnecting(plugIndex: number, position: Position): void;
+    onStopConnecting(plugIndex: number, position: Position): void;
+    onPlugPositions(plugPositions: Position[]): void;
+}
+
+const ComponentGraphCanvasNode = ({ node, position, onDragStart, onStartConnecting, onStopConnecting, onPlugPositions }: ComponentGraphCanvasNodeProps) => {
+    const [plugPositions, setPlugPositions] = React.useState<Position[]>([]);
+
+    const handlePosition = (i: number) => (plugPosition: Position) => {
+            setPlugPositions(plugPositions => {
+                const plugPositionsCopy = [...plugPositions];
+    
+                plugPositionsCopy[i] = plugPosition;
+    
+                return plugPositionsCopy;
+            });
+    };
+
+    React.useEffect(() => {
+        if (plugPositions.length > 0 && plugPositions.every(plugPosition => plugPosition !== undefined)) {
+            onPlugPositions(plugPositions);
+        }
+    }, [plugPositions]);
+
+    return (
+        <div style={{border: '1px solid gray', position: 'absolute', ...position, }}>
+            <div onMouseDown={onDragStart} style={{padding: '5px 10px', borderBottom: '1px solid black', cursor: 'move'}}>{node.name}</div>
+            <div style={{padding: '10px 10px'}}>
+                {node.inPlugs.map((_, i) => <NodePlug onPosition={handlePosition(i)} onStartConnecting={position => onStartConnecting(i, position)} onStopConnecting={position => onStopConnecting(i, position)} />)}
+            </div>
+            <div style={{cursor: 'initial', padding: 10}}>{node.component}</div>
+            <div style={{padding: '10px 10px'}}>
+                {node.inPlugs.map((_, i) => <NodePlug onPosition={handlePosition(node.inPlugs.length + i)} onStartConnecting={position => onStartConnecting(node.inPlugs.length + i, position)} onStopConnecting={position => onStopConnecting(node.inPlugs.length + i, position)} />)}
+            </div>
         </div>
-    )}</div>;
+    );
+}
+
+interface NodePlugProps {
+    onStartConnecting(position: Position): void;
+    onStopConnecting(position: Position): void;
+    onPosition(position: Position): void;
+}
+
+const NodePlug = ({ onStartConnecting, onStopConnecting, onPosition }: NodePlugProps) => {
+    const [, setPosition] = React.useState<Position>({ top: 0, left: 0});
+
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        if(ref.current) {
+            const newPosition = {
+                top: ref.current.getBoundingClientRect().top,
+                left: ref.current.getBoundingClientRect().left,
+            };
+
+            onPosition(newPosition);
+            setPosition(newPosition);
+        }
+    }, []);
+
+    return (
+        <div 
+            ref={ref}
+            onMouseDown={e => onStartConnecting({top: e.clientY, left: e.clientX})} 
+            onMouseUp={e => onStopConnecting({top: e.clientY, left: e.clientX})} 
+            style={{ backgroundColor: 'lightgray', width: 15, height: 15, borderRadius: 15, border: '1px solid gray' }}
+            />
+    );
 }
