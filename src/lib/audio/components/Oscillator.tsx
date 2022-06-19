6dbeeb5e4d0b3f525be3f5720a110ae08df12852
Mateusz Podlasin <podlas29@gmail.com>
2022-06-19 14:52:22 +0200

    change oscillator wave shape with select

--- src/lib/audio/components/Oscillator.tsx
+++ src/lib/audio/components/Oscillator.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 
+const OSCILLATOR_TYPES: OscillatorType[] = ["sine", "square", "sawtooth", "triangle"];
+
 export function Oscillator({ audioElement: oscillator, audioContext, }: { audioElement: OscillatorNode, audioContext: AudioContext, }) {
   React.useEffect(() => {
-    oscillator.type = 'sine';
+    oscillator.type = OSCILLATOR_TYPES[0];
     oscillator.frequency.value = 440;
     oscillator.start();
   }, [oscillator]);
 
-    
+  const handleOscillatorTypeClick = (e: React.FormEvent<HTMLSelectElement>) => {
+    oscillator.type = e.currentTarget.value as OscillatorType;
+  }
+
   const changeFrequency: React.FormEventHandler<HTMLInputElement> = (e) => {
     oscillator.frequency.setValueAtTime(e.currentTarget.valueAsNumber, audioContext.currentTime);
   };
 
   return <div>
-    <input type="range" min={0} max={2000} step={1} defaultValue={0} onInput={changeFrequency} />
-    <div>{oscillator.type}</div>
+    <select onChange={handleOscillatorTypeClick}>
+      {OSCILLATOR_TYPES.map(oscillatorType => (
+        <option key={oscillatorType} value={oscillatorType}>
+          {oscillatorType}
+        </option>
+      ))}
+    </select>
+    <div>
+    <input onChange={changeFrequency} type="range" min="0" max="1000" step="1" defaultValue={oscillator.frequency.value} />
+    </div>
   </div>;
 }
